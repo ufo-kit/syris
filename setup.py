7a6694bc9836e20f60de9342c0174463c732f845
Tomas Farago <tomas.farago@kit.edu>
2014-04-02 15:28:42 +0200

    Add kernel files to installation

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author_email='tomas.farago@kit.edu',
     license='LGPL',
     packages=find_packages(exclude=['*.tests']),
+    package_data={'syris': ['gpu/opencl/*.cl']},
     # exclude_package_data={'': ['README.rst']},
     description="X-ray imaging simulation",
     # long_description=open('README.rst').read(),
