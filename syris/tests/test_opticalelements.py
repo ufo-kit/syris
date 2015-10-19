7b30619b3eecbf06d6f5abfe31d30ec88f1da54a
Tomas Farago <tomas.farago@kit.edu>
2015-10-19 16:43:31 +0200

    Add OpticalElement test

---
+++ syris/tests/test_opticalelements.py
@@ -0,0 +1,26 @@
+import numpy as np
+import quantities as q
+import syris
+from syris.graphicalobjects import StaticGraphicalObject
+from syris.materials import Material
+from syris.opticalelements import OpticalElement
+from syris.physics import energy_to_wavelength, transfer
+from syris.tests import SyrisTest, slow
+
+
+@slow
+class TestOpticalElement(SyrisTest):
+
+    def setUp(self):
+        syris.init()
+        self.go = StaticGraphicalObject(np.arange(4 ** 2).reshape(4, 4) * q.um)
+        energies = range(10, 20) * q.keV
+        self.energy = energies[len(energies) / 2]
+        self.material = Material('foo', np.arange(len(energies), dtype=np.complex), energies)
+        self.elem = OpticalElement(self.go, self.material)
+
+    def test_transfer(self):
+        transferred = self.elem.transfer(self.energy).get()
+        gt = transfer(self.go.thickness, self.material.get_refractive_index(self.energy),
+                      energy_to_wavelength(self.energy)).get()
+        np.testing.assert_almost_equal(gt, transferred)
