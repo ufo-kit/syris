9b10efda96ea0b5ae015f8062102a780be475663
Tomas Farago <tomas.farago@kit.edu>
2015-11-02 14:56:03 +0100

    tests: More comprehensive test_init

--- syris/tests/test_init.py
+++ syris/tests/test_init.py
@@ -1,7 +1,13 @@
+import logging
 import syris
+import syris.physics
+import syris.profiling
 from syris import config as cfg
 
 
 def test_init():
-    syris.init()
+    syris.init(profiling=True, loglevel=logging.DEBUG, double_precision=True)
+    assert logging.DEBUG == syris.physics.LOG.getEffectiveLevel()
     assert cfg.OPENCL.ctx is not None
+    assert cfg.PRECISION.cl_float == 8
+    assert syris.profiling.PROFILER is not None
