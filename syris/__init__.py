26e503f3b452be13a4b9366dc4ad9dc9428510b2
Tomas Farago <tomas.farago@kit.edu>
2015-11-02 14:26:44 +0100

    init: Set precision by init

--- syris/__init__.py
+++ syris/__init__.py
@@ -2,7 +2,7 @@
 import atexit
 import logging
 import pyopencl as cl
-from syris.config import OPENCL, PROFILED_CL_FUNCTIONS, init_logging
+from syris.config import OPENCL, PRECISION, PROFILED_CL_FUNCTIONS, init_logging
 from syris.gpu.util import make_opencl_defaults, init_programs, execute_profiled
 from syris import profiling as prf
 
@@ -11,8 +11,9 @@ __version__ = '0.1'
 
 
 def init(device_index=0, profiling=True, profiling_file='profile.dat', loglevel=logging.DEBUG,
-         logfile=None):
+         logfile=None, double_precision=False):
     """Initialize syris with *device_index*."""
+    PRECISION.set_precision(double_precision)
     if OPENCL.ctx is None:
         make_opencl_defaults(device_index=device_index, profiling=profiling)
     if profiling:
