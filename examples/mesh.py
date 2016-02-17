5a9efbff157def339bdec5897295667a1b1272d3
Tomas Farago <sensej007@email.cz>
2016-02-17 16:39:49 +0100

    examples: Add units parameter to meshes

--- examples/mesh.py
+++ examples/mesh.py
@@ -17,15 +17,17 @@ def main():
     """Main function."""
     args = parse_args()
     syris.init(loglevel=logging.INFO)
-    triangles = make_cube() if args.input is None else read_blender_obj(args.input) * q.m
-    tr = geom.Trajectory([(0, 0, 0)] * q.m)
+    units = q.Quantity(1, args.units)
+    triangles = make_cube().magnitude if args.input is None else read_blender_obj(args.input)
+    triangles = triangles * units
+    tr = geom.Trajectory([(0, 0, 0)] * units)
     mesh = Mesh(triangles, tr, center=args.center)
     LOG.info('Number of triangles: {}'.format(mesh.num_triangles))
 
     shape = (args.n, args.n)
     if args.pixel_size is None:
         if args.input is None:
-            fov = 4. * q.m
+            fov = 4. * units
         else:
             # Maximum sample size in x and y direction
             max_diff = np.max(mesh.extrema[:-1, 1] - mesh.extrema[:-1, 0])
@@ -40,13 +42,13 @@ def main():
     else:
         translate = (args.translate[0].simplified.magnitude,
                      args.translate[1].simplified.magnitude, 0) * q.m
-    LOG.info('Translation: {}'.format(translate))
+    LOG.info('Translation: {}'.format(translate.rescale(q.um)))
 
     mesh.translate(translate)
     mesh.rotate(args.y_rotate, geom.Y_AX)
     mesh.rotate(args.x_rotate, geom.X_AX)
     fmt = 'n: {}, pixel size: {}, FOV: {}'
-    LOG.info(fmt.format(args.n, args.pixel_size.simplified, fov.simplified))
+    LOG.info(fmt.format(args.n, args.pixel_size.rescale(q.um), fov.rescale(q.um)))
 
     proj = mesh.project(shape, args.pixel_size, t=None).get()
     offset = syris.gpu.util.make_vfloat3(0, translate[1].simplified, -(fov / 2.).simplified)
@@ -73,6 +75,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
 
     parser.add_argument('--input', type=str, help='Input .obj file')
+    parser.add_argument('--units', type=str, default='um', help='Mesh physical units')
     parser.add_argument('--n', type=int, default=256, help='Number of pixels')
     parser.add_argument('--pixel-size', type=float, help='Pixel size in um')
     parser.add_argument('--center', type=str, help='Mesh centering on creation')
