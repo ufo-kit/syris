2516b9aad2c3206c5663f5e8f4101982bb787b0e
Tomas Farago <sensej007@email.cz>
2016-06-14 11:28:26 +0200

    examples: Clean up metaballs

--- examples/metaballs.py
+++ examples/metaballs.py
@@ -1,62 +1,18 @@
 import logging
 import re
-from matplotlib import pyplot as plt, cm
+from matplotlib import pyplot as plt
 import numpy as np
 import pyopencl as cl
-from pyopencl.array import vec
 import quantities as q
 import syris
 from syris import config as cfg
-from syris.gpu import util as g_util
 from syris.bodies.isosurfaces import MetaBall, MetaBalls, project_metaballs_naive
 from syris.geometry import Trajectory
 from syris.util import make_tuple, save_image
-from util import show
+from util import get_default_parser, show
 
 
 LOG = logging.getLogger(__name__)
-
-
-UNITS = q.m
-VECTOR_WIDTH = 1
-SUPERSAMPLING = 1
-MAX_OBJECTS = 30
-
-
-def diff(ar):
-    res = []
-    for i in range(1, len(ar)):
-        res.append(np.abs(ar[i] - ar[i - 1]))
-
-    return res
-
-
-def print_array(ar):
-    if len(ar) == 0:
-        return
-
-    res = ""
-
-    for i in range(len(ar)):
-        res += "{:.8f}, ".format(float(ar[i]))
-
-    print res
-
-
-def create_metaball_random(n, pixel_size, radius_range):
-    x = np.random.uniform(0, n * pixel_size)
-    y = np.random.uniform(0, n * pixel_size)
-    # z = np.random.uniform(-1 * q.mm, 1 * q.mm) * radius_range.units
-    z = np.random.uniform(- n / 2 * pixel_size + radius_range[1], n / 2 * pixel_size - radius_range[1])
-    # z = np.random.uniform(radius_range[0], radius_range[1]) * radius_range.units
-    r = np.random.uniform(radius_range[0], radius_range[1]) * radius_range.units
-
-    c_points = [(x, y, z)] * q.mm
-    trajectory = Trajectory(c_points)
-    metaball = MetaBall(trajectory, r)
-    metaball.move(0 * q.s)
-
-    return metaball, "({0}, {1}, {2}, {3}),\n".format(x, y, z, r.magnitude)
 
 
 def load_params(file_name):
@@ -83,9 +39,9 @@ def create_metaballs(params):
     objects = ""
     metaballs = []
     for i in range(len(params)):
-        c_points = [(x[i], y[i], z[i])] * q.mm
+        c_points = [(x[i], y[i], z[i])] * q.um
         trajectory = Trajectory(c_points)
-        metaball = MetaBall(trajectory, r[i] * q.mm)
+        metaball = MetaBall(trajectory, r[i] * q.um)
         metaball.move(0 * q.s)
         metaballs.append(metaball)
         objects += metaball.pack()
@@ -100,31 +56,17 @@ def get_vfloat_mem_host(mem, size):
     return res
 
 
-def create_metaballs_random(num_objects, write_parameters=False):
-    objects_all = ""
-    params_all = ""
-    metaballs = []
-    min_r = SUPERSAMPLING * 5 * pixel_size.rescale(UNITS).magnitude
-    radius_range = (min_r,
-                    SUPERSAMPLING * 50 *
-                    pixel_size.rescale(UNITS).magnitude) * UNITS
-    mid = (radius_range[0].rescale(UNITS).magnitude +
-           radius_range[1].rescale(UNITS).magnitude) / 2
-    coeff = 1.0 / mid
-    eps = pixel_size.rescale(UNITS).magnitude
-    print "mid, coeff, eps:", mid, coeff, eps
-    print "objects:", num_objects
-    for j in range(num_objects):
-        metaball, params = create_metaball_random(n, pixel_size, radius_range)
-        objects_all += metaball.pack()
-        params_all += params
-        metaballs.append(metaball)
+def create_metaballs_random(n, pixel_size, num, min_radius, max_radius):
+    params = []
 
-    if write_parameters:
-        with open("/home/farago/data/params.txt", "w") as f:
-            f.write(params_all + "\n")
+    for j in range(num):
+        x = np.random.uniform(0, n)
+        y = np.random.uniform(0, n)
+        z = np.random.uniform(-2 * max_radius, 2 * max_radius)
+        r = np.random.uniform(min_radius, max_radius)
+        params.append([x, y, z, r])
 
-    return metaballs, objects_all, coeff, mid
+    return create_metaballs(params)
 
 
 def get_z_range(metaballs):
@@ -145,13 +87,14 @@ def get_z_range(metaballs):
 
 def create_metaball_buffers(n, thickness):
     if thickness:
-        res = np.empty((n, VECTOR_WIDTH * n), dtype=cfg.PRECISION.np_float)
+        res = np.empty((n, n), dtype=cfg.PRECISION.np_float)
         result_mem_size = n ** 2 * cfg.PRECISION.cl_float
         result_mem = cl.Buffer(cfg.OPENCL.ctx, cl.mem_flags.READ_WRITE,
-                                size=result_mem_size)
+                               size=result_mem_size)
     else:
-        result_mem_size = n ** 2 * 2 * MAX_OBJECTS * cfg.PRECISION.cl_float
-        res = np.empty(MAX_OBJECTS * 2 * n * VECTOR_WIDTH * n, dtype=cfg.PRECISION.np_float)
+        result_mem_size = n ** 2 * 2 * cfg.MAX_META_BODIES * cfg.PRECISION.cl_float
+        res = np.empty(cfg.MAX_META_BODIES * 2 * n * n,
+                       dtype=cfg.PRECISION.np_float)
         res[:] = np.inf
         result_mem = cl.Buffer(cfg.OPENCL.ctx, cl.mem_flags.READ_WRITE |
                                cl.mem_flags.COPY_HOST_PTR, hostbuf=res)
@@ -169,8 +112,8 @@ def intersections_to_slice(n, height, intersections_mem, z_start, pixel_size, pr
                                         slice_mem,
                                         intersections_mem,
                                         np.uint32(height),
-                                        cfg.PRECISION.np_float(z_start.rescale(UNITS).magnitude),
-                                        cfg.PRECISION.np_float(pixel_size.rescale(UNITS).magnitude))
+                                        cfg.PRECISION.np_float(z_start.rescale(q.um).magnitude),
+                                        cfg.PRECISION.np_float(pixel_size.rescale(q.um).magnitude))
     ev.wait()
     print "duration:", (ev.profile.end - ev.profile.start) * 1e-6 * q.ms
 
@@ -178,52 +121,68 @@ def intersections_to_slice(n, height, intersections_mem, z_start, pixel_size, pr
     return slice
 
 
-if __name__ == '__main__':
-    syris.init()
+def parse_args():
+    parser = get_default_parser('Metaballs example')
+    parser.add_argument('--n', type=int, default=512, help='Number of image pixels')
+    parser.add_argument('--method', choices=['random', 'fixed', 'file'], default='random',
+                        help='Use random number of metaballs, fixed metaballs or file with '
+                        'packed metaball structure')
+    parser.add_argument('--num', type=int, default=50, help='Number of random metaballs')
+    parser.add_argument('--min-radius', type=int, default=5,
+                        help='Minimum radius of random metaballs in pixels')
+    parser.add_argument('--max-radius', type=int, default=25,
+                        help='Maximum radius of random metaballs in pixels')
+    parser.add_argument('--input', type=str, help='Input file for packed metaballs')
+    parser.add_argument('--output', type=str,
+                        help='Output file for created metaballs as a packed structure')
+    parser.add_argument('--output-thickness', type=str,
+                        help='Output file for projected thickness')
+    parser.add_argument('--algorithm', choices=['naive', 'fast'], default='fast',
+                        help='Used algorithm for intersections computation')
 
-    pixel_size = 1e-3 / SUPERSAMPLING * q.mm
+    return parser.parse_args()
 
-    prg = g_util.get_program(g_util.get_metaobjects_source())
-    n = SUPERSAMPLING * 512
 
-    # params = load_params('/home/farago/data/params.txt')
-    # # params = [(0.25, 0.25, 0.0, 0.05), (0.35, 0.35, 0.0, 0.1)]
-    # num_objects = len(params)
-    # balls, objects_all = create_metaballs(params)
-    # z_min, z_max = get_z_range(balls)
+def main():
+    args = parse_args()
+    syris.init(device_index=0)
+    shape = (args.n, args.n)
+    pixel_size = 1 * q.um
 
-    # Random metaballs creation
-    # num_objects = np.random.randint(1, 100)
-    # metaballs, objects_all, coeff, mid = create_metaballs_random(num_objects)
-    positions = [(n / 4, n / 2, 0, n / 5),
-                 (3 * n / 4, n / 2, 0, n / 5)] * pixel_size.rescale(q.mm).magnitude
-    num_objects = len(positions)
-    print positions
-    metaballs, objects_all = create_metaballs(positions)
+    if args.method == 'random':
+        # Random metaballs creation
+        metaballs, objects_all = create_metaballs_random(args.n, pixel_size, args.num,
+                                                         args.min_radius, args.max_radius)
+    elif args.method == 'file':
+        # 1e6 because packing converts to meters
+        values = np.fromfile(args.input, dtype=np.float32) * 1e6
+        metaballs, objects_all = create_metaballs(values.reshape(len(values) / 4, 4))
+    else:
+        positions = [(args.n / 4, args.n / 2, 0, args.n / 5),
+                     (3 * args.n / 4, args.n / 2, 0, args.n / 5)]
+        metaballs, objects_all = create_metaballs(positions)
+
+    if args.output:
+        with open(args.output, mode='wb') as out_file:
+            out_file.write(objects_all)
+
     z_min, z_max = get_z_range(metaballs)
-    # print coeff, mid
-    print 'z min, max:', z_min, z_max, n * pixel_size + z_min, UNITS
-    print 'Z steps:', ((z_max - z_min) / pixel_size).simplified
+    print 'z min, max:', z_min.rescale(q.um), z_max.rescale(q.um), args.n * pixel_size + z_min
 
-    objects_mem = cl.Buffer(cfg.OPENCL.ctx, cl.mem_flags.READ_ONLY |
-                            cl.mem_flags.COPY_HOST_PTR, hostbuf=objects_all)
+    if args.algorithm == 'fast':
+        traj = Trajectory([(0, 0, 0)] * q.m)
+        comp = MetaBalls(traj, metaballs)
+        thickness = comp.project(shape, pixel_size).get()
+    else:
+        print 'Z steps:', int(((z_max - z_min) / pixel_size).simplified.magnitude + 0.5)
+        thickness = project_metaballs_naive(metaballs, shape, make_tuple(pixel_size)).get()
 
-    traj = Trajectory([(0, 0, 0)] * q.m)
-    comp = MetaBalls(traj, metaballs)
-    # thickness = comp.project((n, n), pixel_size).get()
-    thickness = project_metaballs_naive(metaballs, (n, n), make_tuple(pixel_size)).get()
+    if args.output_thickness:
+        save_image(args.output_thickness, thickness)
 
-    objects_mem.release()
-
-    # for h in range(n):
-    #     res = intersections_to_slice(n, h, res_mem, z_min, pixel_size, prg)
-    #     save_image("/home/farago/data/thickness/radio_{:>05}.tif".format(h),
-    #                res[:, ::VECTOR_WIDTH])
-
-    show(thickness[:, ::VECTOR_WIDTH])
-
-    # plt.figure()
-    # plt.imshow(res1[:, ::VECTOR_WIDTH], origin="lower", cmap=cm.get_cmap("gray"),
-    #            interpolation="nearest")
-    # plt.colorbar()
+    show(thickness)
     plt.show()
+
+
+if __name__ == '__main__':
+    main()
