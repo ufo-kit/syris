39f1a23aa84b2b7770bb0151da4e6f3777f862c7
Tomas Farago <tomas.farago@kit.edu>
2016-12-01 11:33:50 +0100

    tests: Don't use get_cuda_context

--- syris/tests/test_precision.py
+++ syris/tests/test_precision.py
@@ -5,9 +5,6 @@ import syris.config as cfg
 from syris.gpu import util as gpu_util
 from syris.tests import SyrisTest
 
-ctx = gpu_util.get_cuda_context()
-queue = gpu_util.get_command_queues(ctx)[0]
-
 
 class TestPrecision(SyrisTest):
 
@@ -16,20 +13,20 @@ class TestPrecision(SyrisTest):
         self.n = 2
         self.kernel_fn = "vfloat_test.cl"
 
-    def _create_mem_objs(self, ctx, n):
-        mem = cl.Buffer(ctx, cl.mem_flags.READ_WRITE, size=n * cfg.PRECISION.cl_float)
+    def _create_mem_objs(self, n):
+        mem = cl.Buffer(cfg.OPENCL.ctx, cl.mem_flags.READ_WRITE, size=n * cfg.PRECISION.cl_float)
         ar = np.empty(n, dtype=cfg.PRECISION.np_float)
 
         return mem, ar
 
     def _execute_and_check(self):
-        prg = cl.Program(ctx, gpu_util.get_source([self.kernel_fn])).build()
-        mem, ar = self._create_mem_objs(ctx, self.n)
-        prg.float_test(queue,
+        prg = cl.Program(cfg.OPENCL.ctx, gpu_util.get_source([self.kernel_fn])).build()
+        mem, ar = self._create_mem_objs(self.n)
+        prg.float_test(cfg.OPENCL.queue,
                        (self.n,),
                        None,
                        mem)
-        cl.enqueue_copy(queue, ar, mem)
+        cl.enqueue_copy(cfg.OPENCL.queue, ar, mem)
         res = ar == np.array([0, 1], dtype=cfg.PRECISION.np_float)
         self.assertTrue(res.all())
 
--- syris/tests/test_vcomplex.py
+++ syris/tests/test_vcomplex.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pyopencl as cl
+import syris
 from syris.gpu import util as gpu_util
 from syris import config as cfg
 from syris.tests import SyrisTest, slow
@@ -9,38 +10,37 @@ from syris.tests import SyrisTest, slow
 class TestVComplex(SyrisTest):
 
     def _execute_kernel(self, kernel):
-        kernel(self.queue, (1,), None, self.mem_0, self.mem_1, self.mem_out)
+        kernel(cfg.OPENCL.queue, (1,), None, self.mem_0, self.mem_1, self.mem_out)
 
     def setUp(self):
-        self.ctx = gpu_util.get_cuda_context()
-        self.queue = gpu_util.get_command_queues(self.ctx)[0]
+        syris.init()
         self.num_0 = np.array([17 - 38j], dtype=cfg.PRECISION.np_cplx)
         self.num_1 = np.array([-135 + 563j], dtype=cfg.PRECISION.np_cplx)
-        self.mem_0 = cl.Buffer(self.ctx, cl.mem_flags.READ_WRITE |
+        self.mem_0 = cl.Buffer(cfg.OPENCL.ctx, cl.mem_flags.READ_WRITE |
                                cl.mem_flags.COPY_HOST_PTR, hostbuf=self.num_0)
-        self.mem_1 = cl.Buffer(self.ctx, cl.mem_flags.READ_WRITE |
+        self.mem_1 = cl.Buffer(cfg.OPENCL.ctx, cl.mem_flags.READ_WRITE |
                                cl.mem_flags.COPY_HOST_PTR, hostbuf=self.num_1)
-        self.mem_out = cl.Buffer(self.ctx, cl.mem_flags.READ_WRITE, cfg.PRECISION.cl_cplx)
+        self.mem_out = cl.Buffer(cfg.OPENCL.ctx, cl.mem_flags.READ_WRITE, cfg.PRECISION.cl_cplx)
         self.host_array = np.empty(1, cfg.PRECISION.np_cplx)
         src = gpu_util.get_source(["vcomplex.cl"])
-        self.prg = cl.Program(self.ctx, src).build()
+        self.prg = cl.Program(cfg.OPENCL.ctx, src).build()
 
     def test_addition(self):
         self._execute_kernel(self.prg.vc_add_kernel)
-        cl.enqueue_copy(self.queue, self.host_array, self.mem_out)
+        cl.enqueue_copy(cfg.OPENCL.queue, self.host_array, self.mem_out)
         self.assertAlmostEqual(self.host_array[0], self.num_0 + self.num_1)
 
     def test_subtraction(self):
         self._execute_kernel(self.prg.vc_sub_kernel)
-        cl.enqueue_copy(self.queue, self.host_array, self.mem_out)
+        cl.enqueue_copy(cfg.OPENCL.queue, self.host_array, self.mem_out)
         self.assertAlmostEqual(self.host_array[0], self.num_0 - self.num_1)
 
     def test_multiplication(self):
         self._execute_kernel(self.prg.vc_mul_kernel)
-        cl.enqueue_copy(self.queue, self.host_array, self.mem_out)
+        cl.enqueue_copy(cfg.OPENCL.queue, self.host_array, self.mem_out)
         self.assertAlmostEqual(self.host_array[0], self.num_0 * self.num_1)
 
     def test_division(self):
         self._execute_kernel(self.prg.vc_div_kernel)
-        cl.enqueue_copy(self.queue, self.host_array, self.mem_out)
+        cl.enqueue_copy(cfg.OPENCL.queue, self.host_array, self.mem_out)
         self.assertAlmostEqual(self.host_array[0], self.num_0 / self.num_1)
