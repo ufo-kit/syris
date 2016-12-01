e7a6aea159a6b8d5673c3e6ee72c52a3f897fdb3
Tomas Farago <tomas.farago@kit.edu>
2016-12-01 11:32:45 +0100

    tests: Update function parameters in OE

--- syris/tests/test_opticalelements.py
+++ syris/tests/test_opticalelements.py
@@ -10,7 +10,8 @@ from syris.tests import SyrisTest, slow
 
 class DummyOpticalElement(OpticalElement):
 
-    def _transfer(self, shape, pixel_size, energy, offset, t=0 * q.s, queue=None, out=None):
+    def _transfer(self, shape, pixel_size, energy, offset, t=0 * q.s, queue=None, out=None,
+                  block=False):
         return shape, pixel_size
 
 
