61b4b86dfa4a7de1a797b2dd75edd0791d4706cb
Tomas Farago <tomas.farago@kit.edu>
2017-01-11 14:37:07 +0100

    filters: Add Gaussian filter

--- syris/devices/filters.py
+++ syris/devices/filters.py
@@ -8,6 +8,53 @@ import scipy.interpolate as interp
 import syris.config as cfg
 from syris.opticalelements import OpticalElement
 from syris.physics import energy_to_wavelength
+from syris.util import get_gauss
+
+
+class Filter(OpticalElement):
+
+    """Beam frequency filter."""
+
+    def get_next_time(self, t_0, distance):
+        """A filter doesn't move, this function returns infinity."""
+        return np.inf * q.s
+
+
+class GaussianFilter(OpticalElement):
+
+    """Gaussian beam filter."""
+
+    def __init__(self, energies, center, sigma, peak_transmission=1):
+        """Create a Gussian beam filter for *energies* [keV], center it at *center* [keV] and use
+        std *sigma* [keV]. *peak_transmission* specifies the transmitted intensity for energy
+        *center*, i.e. this is the highest transmitted intensity.
+        """
+        if len(energies) < 4:
+            raise ValueError('Number of energy points too low for interpolation')
+        energies = energies.rescale(q.keV).magnitude
+        center = center.rescale(q.keV).magnitude
+        sigma = sigma.rescale(q.keV).magnitude
+        profile = get_gauss(energies, center, sigma) * peak_transmission * q.keV
+        self._tck = interp.splrep(energies, profile)
+
+    def get_next_time(self, t_0, distance):
+        """A filter doesn't move, this function returns infinity."""
+        return np.inf * q.s
+
+    def _transfer(self, shape, pixel_size, energy, offset, exponent=False, t=None, queue=None,
+                  out=None, check=True, block=False):
+        """Transfer function implementation. Only *energy* is relevant because a filter has the same
+        thickness everywhere.
+        """
+        coeff = interp.splev(energy.rescale(q.keV).magnitude, self._tck)
+        eps = np.finfo(cfg.PRECISION.np_float).eps
+
+        if exponent:
+            result = np.log(coeff) / 2 if np.abs(coeff) > eps else -np.inf
+        else:
+            result = np.sqrt(coeff) if np.abs(coeff) > eps else 0.0
+
+        return cfg.PRECISION.np_cplx(result)
 
 
 class MaterialFilter(Filter):
