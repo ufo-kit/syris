604ea72ea5d99162c30c1bf5df176fed6a247718
Tomas Farago <tomas.farago@kit.edu>
2017-01-11 14:37:22 +0100

    tests: Test Gaussian filter

--- syris/tests/test_filters.py
+++ syris/tests/test_filters.py
@@ -1,8 +1,9 @@
 import numpy as np
 import quantities as q
 import syris
-from syris.devices.filters import MaterialFilter
+from syris.devices.filters import GaussianFilter, MaterialFilter
 from syris.materials import Material
+from syris.math import fwnm_to_sigma, sigma_to_fwnm
 from syris.physics import energy_to_wavelength
 from syris.tests import SyrisTest, slow
 
@@ -44,3 +45,26 @@ class TestFilters(SyrisTest):
         transmitted = self.fltr.transfer(None, None, self.energy)
         exponent = self.fltr.transfer(None, None, self.energy, exponent=True)
         self.assertAlmostEqual(transmitted, np.exp(k * exponent), places=5)
+
+    def test_gaussian(self):
+        energies = np.arange(5, 30, 0.1) * q.keV
+        energy = 15 * q.keV
+        sigma = fwnm_to_sigma(1, n=2) * q.keV
+        energy_10 = energy - sigma_to_fwnm(sigma.magnitude, n=10) / 2 * q.keV
+        fltr = GaussianFilter(energies, energy, sigma, peak_transmission=0.5)
+        u_0 = 10 + 0j
+
+        u_f = fltr.transfer(None, None, energy)
+        u = u_0 * u_f
+        self.assertAlmostEqual(np.abs(u) ** 2, 50, places=4)
+        u_f = fltr.transfer(None, None, energy_10)
+        u = u_0 * u_f
+        self.assertAlmostEqual(np.abs(u) ** 2, 5, places=4)
+
+        # Test exponent
+        u_f = fltr.transfer(None, None, energy, exponent=True)
+        u = u_0 * np.exp(u_f)
+        self.assertAlmostEqual(np.abs(u) ** 2, 50, places=4)
+        u_f = fltr.transfer(None, None, energy_10, exponent=True)
+        u = u_0 * np.exp(u_f)
+        self.assertAlmostEqual(np.abs(u) ** 2, 5, places=4)
