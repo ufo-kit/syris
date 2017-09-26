c469e3dd304a4a192e4301a8918642db684967df
Tomas Farago <sensej007@email.cz>
2017-09-26 17:31:25 +0200

    docs: Use Mock after path specification

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,12 +20,12 @@ class Mock(MagicMock):
             return MagicMock()
 
 MOCK_MODULES = ['pyfft', 'pyopencl', 'quantities', 'numpy', 'scipy']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 from syris import __version__
 
