7340896d963333179b96af23318e922bd57168f4
Tomas Farago <sensej007@email.cz>
2018-06-04 16:26:30 +0200

    Add transfer_fourier
    for samples which manipulate wavefields in the Fourier space.

--- syris/opticalelements.py
+++ syris/opticalelements.py
@@ -13,12 +13,12 @@ class OpticalElement(object):
 
     def transfer(self, shape, pixel_size, energy, exponent=False, offset=None, t=None,
                  queue=None, out=None, check=True, block=False):
-        """Transfer function of the element on an image plane of size *shape*, use *pixel_size*,
-        *energy*, *offset* is the physical spatial offset of the element as (y, x), transfer at time
-        *t*. If *exponent* is true, compute the exponent of the transfer function without applying
-        the wavenumber. Use *queue* for OpenCL computations and *out* pyopencl array. If *block* is
-        True, wait for the kernel to finish. If *check* is True, the function is checked for
-        aliasing artefacts.
+        """Transfer function of the element in real space on an image plane of size *shape*, use
+        *pixel_size*, *energy*, *offset* is the physical spatial offset of the element as (y, x),
+        transfer at time *t*. If *exponent* is true, compute the exponent of the transfer function
+        without applying the wavenumber. Use *queue* for OpenCL computations and *out* pyopencl
+        array. If *block* is True, wait for the kernel to finish. If *check* is True, the function
+        is checked for aliasing artefacts.
         """
         shape = make_tuple(shape, num_dims=2)
         pixel_size = make_tuple(pixel_size, num_dims=2)
@@ -30,8 +30,27 @@ class OpticalElement(object):
         return self._transfer(shape, pixel_size, energy, offset, exponent=exponent, t=t,
                               queue=queue, out=out, check=check, block=block)
 
+    def transfer_fourier(self, shape, pixel_size, energy, t=None, queue=None,
+                         out=None, block=False):
+        """Transfer function of the element in Fourier space of size *shape*, use *pixel_size*,
+        *energy* and comput the function at time *t*. Use *queue* for OpenCL computations and *out*
+        pyopencl array. If *block* is True, wait for the kernel to finish.
+        """
+        shape = make_tuple(shape, num_dims=2)
+        pixel_size = make_tuple(pixel_size, num_dims=2)
+        if queue is None:
+            queue = cfg.OPENCL.queue
+
+        return self._transfer_fourier(shape, pixel_size, energy, t=t, queue=queue, out=out,
+                                      block=block)
+
     def _transfer(self, shape, pixel_size, energy, offset, exponent=False, t=None,
                   queue=None, out=None, check=True, block=False):
+        """Transfer function implementation."""
+        raise NotImplementedError
+
+    def _transfer_fourier(self, shape, pixel_size, energy, t=None, queue=None,
+                          out=None, block=False):
         """Transfer function implementation."""
         raise NotImplementedError
 
