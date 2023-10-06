a46c2a286ce7cd0a86cb7c6755734b2bb69df564
Tomas Farago <sensej007@email.cz>
2023-10-06 10:49:51 +0200

    Add copyright notice

--- examples/transmission_function_sampling.py
+++ examples/transmission_function_sampling.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2013-2023 Karlsruhe Institute of Technology
+#
+# This file is part of syris.
+#
+# This library is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not, see <http://www.gnu.org/licenses/>.
+
 """Aliasing of the transmission function of a wedge which projection is computed as f(x, y) = x.
 Delta is chosen in such a way that it causes phase shift between two adjacent pixels 2Pi in case of
 no supersampling. Thus, The transmission function of the wedge along x has phase 0, 2Pi, 4Pi, ...,
