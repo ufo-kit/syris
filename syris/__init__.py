ddf0a13856987ad2258054961f551c00cc8a22b0
Tomas Farago <sensej007@email.cz>
2024-02-16 14:35:58 +0100

    init: do not pull packages in

--- syris/__init__.py
+++ syris/__init__.py
@@ -16,18 +16,9 @@
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
 
 """Synchrotron Radiation Imaging Simulation (SYRIS) initialization."""
-import atexit
-import logging
-import pyopencl as cl
-import syris.config as cfg
-from syris.gpu.util import make_opencl_defaults, init_programs, execute_profiled
-from syris import profiling as prf
 
 
 __version__ = "0.4dev"
-
-
-LOG = logging.getLogger(__name__)
 
 
 def init(
@@ -36,12 +27,21 @@ def init(
     device_index=None,
     profiling=False,
     profiling_file="profile.dat",
-    loglevel=logging.INFO,
+    loglevel=None,
     logfile=None,
     double_precision=False,
 ):
     """Initialize syris with *device_index*."""
-    cfg.init_logging(level=loglevel, logger_file=logfile)
+    import atexit
+    import logging
+    import pyopencl as cl
+    import syris.config as cfg
+    from syris.gpu.util import make_opencl_defaults, init_programs
+    from syris import profiling as prf
+
+    LOG = logging.getLogger(__name__)
+
+    cfg.init_logging(level=logging.INFO if loglevel is None else loglevel, logger_file=logfile)
     cfg.PRECISION = cfg.Precision(double_precision)
     cfg.OPENCL = cfg.OpenCL()
     platforms = []
@@ -74,5 +74,9 @@ def init(
 
 
 def _wrap_opencl():
+    import pyopencl as cl
+    import syris.config as cfg
+    from syris.gpu.util import execute_profiled
+
     for function in cfg.PROFILED_CL_FUNCTIONS:
         setattr(cl, function.__name__, execute_profiled(function))
