013b3b75037352be5f5a2a97ba3f229cc6781c1f
Tomas Farago <sensej007@email.cz>
2024-02-22 16:29:49 +0100

    spheres: switch to hydra

--- examples/spheres.py
+++ examples/spheres.py
@@ -20,13 +20,16 @@ Creation of spheres in a capillary, their projection and X-ray simulation.
 """
 import functools
 import glob
+import hydra
 import imageio
 import multiprocessing
 import numpy as np
+import pyopencl.array as cl_array
 import os
 import quantities as q
 import syris
 import tqdm
+from omegaconf import OmegaConf
 from syris.bodies.simple import StaticBody
 from syris.devices.sources import make_topotomo
 from syris.devices.cameras import Camera
@@ -36,7 +39,7 @@ from syris.imageprocessing import decimate
 from syris.math import fwnm_to_sigma
 from syris.physics import propagate, energy_to_wavelength
 from multiprocessing.pool import Pool
-from .util import get_default_parser, get_material
+from .util import get_material
 
 
 def project_sphere(xc, yc, radius):
@@ -101,40 +104,36 @@ def make_cylinder_profile(n, radius):
     return np.tile(profile, [n, 1])
 
 
-def make_spheres(
-    n=512,
-    n_spheres=1000,
-    inner_cylinder_radius=None,
-    r_min=2,
-    r_max=12,
-    rho_min=0.5,
-    rho_max=1.0,
-    output_directory=None,
-):
+def make_spheres(common):
+    args = common.create
     """Create sphere locations, sizes and densities."""
-    if rho_min <= 0:
+    if args.rho_min <= 0:
         raise ValueError("Minimum relative density must be > 0")
-    if rho_max > 1:
+    if args.rho_max > 1:
         raise ValueError("Maximum relative density must be <= 1")
 
-    x_0 = n // 2 - inner_cylinder_radius
-    x_1 = n // 2 + inner_cylinder_radius
+    x_0 = common.n // 2 - common.inner_cylinder_radius
+    x_1 = common.n // 2 + common.inner_cylinder_radius
     y_0 = 0
-    y_1 = n
+    y_1 = common.n
 
-    x = np.random.randint(x_0 + r_max, high=x_1 - r_max, size=n_spheres)
-    z = np.random.randint(x_0 - n // 2 + r_max, high=x_1 - n // 2 - r_max, size=n_spheres)
-    y = np.random.randint(y_0, high=y_1, size=n_spheres)
-    if r_min == r_max:
-        r = np.ones(n_spheres) * r_min
+    x = np.random.randint(x_0 + args.r_max, high=x_1 - args.r_max, size=args.n_spheres)
+    z = np.random.randint(
+        x_0 - common.n // 2 + args.r_max,
+        high=x_1 - common.n // 2 - args.r_max,
+        size=args.n_spheres
+    )
+    y = np.random.randint(y_0, high=y_1, size=args.n_spheres)
+    if args.r_min == args.r_max:
+        r = np.ones(args.n_spheres) * args.r_min
     else:
-        r = np.random.randint(r_min, r_max, size=n_spheres)
+        r = np.random.randint(args.r_min, args.r_max, size=args.n_spheres)
 
     # Spheres overlapping test
-    valid = np.ones(n_spheres, dtype=bool)
+    valid = np.ones(args.n_spheres, dtype=bool)
     ind = np.arange(len(valid))
 
-    for i in tqdm.tqdm(range(n_spheres - 1)):
+    for i in tqdm.tqdm(range(args.n_spheres - 1)):
         if not valid[i]:
             continue
         vi = np.where((ind > i) & valid)[0]
@@ -156,8 +155,8 @@ def make_spheres(
     zv = z[ind]
     rv = r[ind]
 
-    dist = np.sqrt((xv - n // 2) ** 2 + zv ** 2) + rv
-    iind = np.where(inner_cylinder_radius < dist)[0]
+    dist = np.sqrt((xv - common.n // 2) ** 2 + zv ** 2) + rv
+    iind = np.where(common.inner_cylinder_radius < dist)[0]
     valid[ind[iind]] = False
     ind = np.where(valid)[0]
     print(f"Number of non-overlapping spheres not reaching beyond max-dist: {len(ind)}")
@@ -175,102 +174,88 @@ def make_spheres(
 
         assert np.all(dist_s >= dist_r)
 
-    if rho_min == rho_max:
-        rho = np.ones(len(ind)) * rho_min
+    if args.rho_min == args.rho_max:
+        rho = np.ones(len(ind)) * args.rho_min
     else:
-        rho = np.random.uniform(low=rho_min, high=rho_max, size=len(ind))
+        rho = np.random.uniform(low=args.rho_min, high=args.rho_max, size=len(ind))
     spheres = np.stack((x[ind], y[ind], z[ind], rho, r[ind])).T
 
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory, exist_ok=True)
+    if not os.path.exists(args.output_directory):
+        os.makedirs(args.output_directory, exist_ok=True)
     np.save(
         os.path.join(
-            output_directory,
-            f"spheres-n-{n}-ns-{n_spheres}-{len(ind)}-r-{r_min}-{r_max}-rho-{rho_min}-{rho_max}.npy"
+            args.output_directory,
+            f"spheres-n-{common.n}-ns-{args.n_spheres}-{len(ind)}-r-{args.r_min}"
+            f"-{args.r_max}-rho-{args.rho_min}-{args.rho_max}.npy"
         ),
         spheres
     )
 
 
-def project_spheres(
-    spheres_filename=None,
-    n=None,
-    supersampling=None,
-    num_projections=None,
-):
-    if not num_projections:
-        num_projections = int(np.ceil(np.pi / 2 * n))
-    n_hd = n * supersampling
+def project_spheres(common):
+    args = common.project
+    if not args.num_projections:
+        args.num_projections = int(np.ceil(np.pi / 2 * common.n))
+    n_hd = common.n * args.supersampling
 
-    print(f"Number of projections: {num_projections}")
+    print(f"Number of projections: {args.num_projections}")
 
     mfunc = functools.partial(
         make_projection,
-        spheres_filename,
+        args.spheres_filename,
         n_hd,
-        supersampling,
-        num_projections,
+        args.supersampling,
+        args.num_projections,
     )
 
     with Pool(processes=multiprocessing.cpu_count() - 1) as pool:
         list(
             tqdm.tqdm(
-                pool.imap(mfunc, np.arange(num_projections)),
-                total=num_projections
+                pool.imap(mfunc, np.arange(args.num_projections)),
+                total=args.num_projections
             )
         )
 
 
-def get_camera_image(image, camera, xray_gain, with_noise=False):
+def get_camera_image(image, camera, xray_gain, noise=False):
     """Convert an X-ray *image* to a visible light image."""
     # X-ray -> visible light
-    if with_noise:
+    if noise:
         image = np.random.poisson(image)
     image = image * xray_gain
 
     # Visible light -> electrons -> counts
     return camera.get_image(
         image,
-        shot_noise=with_noise,
-        amplifier_noise=with_noise,
+        shot_noise=noise,
+        amplifier_noise=noise,
         psf=True,
     )
 
 
-def create_xray_projections(
-    n=None,
-    propagation_distance=None,
-    supersampling=None,
-    inner_cylinder_radius=None,
-    outer_cylinder_radius=None,
-    projections_fmt=None,
-    spots_filename=None,
-    max_absorbed_photons=None,
-    with_noise=False,
-    output_suffix=None,
-):
+def create_xray_projections(common):
+    args = common.xray
     syris.init()
-    projection_filenames = sorted(glob.glob(projections_fmt))
+    projection_filenames = sorted(glob.glob(args.projections_fmt))
     n_hd = imageio.imread(projection_filenames[0]).shape[0]
-    supersampling = n_hd / n
+    supersampling = n_hd / common.n
     # Compute grid
     shape = (n_hd, n_hd)
     energy = 15 * q.keV
     lam = energy_to_wavelength(energy)
-    propagation_distance = propagation_distance * q.cm
+    propagation_distance = args.propagation_distance * q.cm
     ps = 1 * q.um
     ps_hd = ps / supersampling
     material = get_material("synth-delta-1e-6-beta-1e-8.mat")
     ri = material.get_refractive_index(energy)
     max_intensity = 4000
-    max_absorbed_photons = max_absorbed_photons
     xray_gain = 20  # number of emitted visible light photons / one X-ray photon
 
     fmt = "Wavelength: {}"
     print(fmt.format(lam))
     fmt = "Pixel size used for propagation: {}"
     print(fmt.format(ps_hd.rescale(q.um)))
-    print("Field of view: {}".format(n * ps.rescale(q.um)))
+    print("Field of view: {}".format(common.n * ps.rescale(q.um)))
     print("Supersampling: {}".format(supersampling))
     print(f"Material mu at {energy}: {material.get_attenuation_coefficient(energy)}")
     print(f"Regularization rate for water in UFO: {np.log10(ri.real / ri.imag)}")
@@ -278,10 +263,10 @@ def create_xray_projections(
     print("Propagator half-size in pixels:", n_kernel_half)
     y_cutoff = max(10, n_kernel_half)
 
-    if not outer_cylinder_radius:
-        outer_cylinder_radius = (n - 50) // 2
-    inner_cylinder = make_cylinder_profile(n_hd, inner_cylinder_radius * supersampling)
-    outer_cylinder = make_cylinder_profile(n_hd, outer_cylinder_radius * supersampling)
+    if not args.outer_cylinder_radius:
+        args.outer_cylinder_radius = (common.n - 50) // 2
+    inner_cylinder = make_cylinder_profile(n_hd, common.inner_cylinder_radius * supersampling)
+    outer_cylinder = make_cylinder_profile(n_hd, args.outer_cylinder_radius * supersampling)
     capillary_thickness = (outer_cylinder - inner_cylinder) * ps_hd
     capillary = StaticBody(capillary_thickness, ps_hd, material=material)
     num_projections = len(projection_filenames)
@@ -289,8 +274,9 @@ def create_xray_projections(
 
     output_directory = os.path.dirname(os.path.dirname(projection_filenames[0]))
     projs_dir = os.path.join(output_directory, "projections")
-    if output_suffix:
-        projs_dir += "-" + output_suffix
+    if args.output_suffix:
+        args.output_suffix = "-" + args.output_suffix
+        projs_dir += args.output_suffix
     os.makedirs(projs_dir, exist_ok=True)
 
     # Devices
@@ -301,7 +287,7 @@ def create_xray_projections(
         500,
         20,
         32,
-        (n, n),
+        (common.n, common.n),
         exp_time=1 * q.ms,
         fps=1000 / q.s,
         quantum_efficiencies=0.5 * np.ones(len(vis_wavelengths)),
@@ -311,60 +297,84 @@ def create_xray_projections(
     # Use fake pixel size to make the bending magnet fall off faster to ~10 % of the instensity at
     # the top and bottom wrt the middle
     source_ps = 20 * q.um
-    source_traj = Trajectory([(n / 2, n / 2, 0)] * source_ps)
+    n_times = 128
+    traj_param = np.linspace(0, args.source.num_periods * 2 * np.pi, n_times, endpoint=False)
+    tx = [common.n / 2] * n_times
+    ty = np.sin(traj_param) * args.source.max_shift * common.n + common.n // 2
+    tz = np.zeros(n_times)
+    t_points = list(zip(tx, ty, tz)) * source_ps
+    source_traj = Trajectory(t_points, pixel_size=source_ps, velocity=source_ps / q.s)
     source = make_topotomo(trajectory=source_traj)
 
     # Flat and dark
-    flat = np.abs(get_host(source.transfer((n, n), source_ps, energy))) ** 2
-    flat = flat / flat.max() * max_absorbed_photons
+    flat = np.abs(get_host(source.transfer((common.n, common.n), source_ps, energy))) ** 2
+    flat = flat / flat.max() * args.max_absorbed_photons
+    flats = []
+    num_flats = 100
 
     imageio.volwrite(
-        os.path.join(output_directory, "flat.tif"),
-        [
-            get_camera_image(
-                flat,
-                camera,
-                xray_gain,
-                with_noise=with_noise
-            )[y_cutoff:-y_cutoff] for i in range(100)
-        ]
-    )
-    imageio.volwrite(
-        os.path.join(output_directory, "dark.tif"),
+        os.path.join(output_directory, f"darks{args.output_suffix}.tif"),
         [
             get_camera_image(
                 np.zeros_like(flat),
                 camera,
                 xray_gain,
-                with_noise=with_noise
-            )[y_cutoff:-y_cutoff] for i in range(100)
+                noise=args.noise
+            )[y_cutoff:-y_cutoff] for i in range(10)
         ]
     )
 
     # Scintillator spots
-    if spots_filename:
-        spots = imageio.imread(spots_filename) if spots_filename else None
+    if args.spots_filename:
+        spots = imageio.imread(args.spots_filename) if args.spots_filename else None
         spots[spots > 0] *= max_intensity
         spots[spots == 0] = 1
         # In case there were some tiny values, they could end up below 1
         spots = np.clip(spots, 1, np.inf)
         flat = np.clip(flat * spots, 0, max_intensity)
 
+    flats_done = False
     # Projections
     for i, filename in tqdm.tqdm(enumerate(projection_filenames)):
+        # Flat field
+        flat_hd = abs(source.transfer(
+            (n_hd, n_hd),
+            source_ps / supersampling,
+            energy,
+            t=i / num_projections * source_traj.time
+        )) ** 2
+        flat_hd /= cl_array.max(flat_hd) / args.max_absorbed_photons
+        flat_ld = decimate(
+            flat_hd,
+            (common.n, common.n),
+            sigma=fwnm_to_sigma(supersampling, n=2),
+            average=True
+        ).get()
+        flat_ld = get_camera_image(flat_ld, camera, xray_gain, noise=args.noise)
+        if not flats_done:
+            if i < num_flats:
+                flats.append(flat_ld[y_cutoff:-y_cutoff])
+            else:
+                imageio.volwrite(
+                    os.path.join(output_directory, f"flats{args.output_suffix}.tif"),
+                    flats
+                )
+                flats_done = True
+
+        # Sample
         spheres = imageio.imread(filename)
         projection = spheres * ps_hd
         sample = StaticBody(projection, ps_hd, material=material)
         # Propagation with a monochromatic plane incident wave
-        hd = propagate([capillary, sample], shape, [energy], propagation_distance, ps_hd).get()
-        ld = flat * decimate(
-            hd,
-            (n, n),
+        hd = propagate([capillary, sample], shape, [energy], propagation_distance, ps_hd)
+        ld = decimate(
+            flat_hd * hd,
+            (common.n, common.n),
             sigma=fwnm_to_sigma(supersampling, n=2),
             average=True
         ).get()
-        ld = get_camera_image(ld, camera, xray_gain, with_noise=with_noise)
-        if spots_filename:
+        ld = get_camera_image(ld, camera, xray_gain, noise=args.noise)
+        if args.spots_filename:
             ld = np.clip(ld * spots, 0, max_intensity)
         imageio.imwrite(
             os.path.join(projs_dir, "projection-{:>05}.tif".format(i)),
@@ -372,122 +382,22 @@ def create_xray_projections(
         )
 
 
-def main():
+@hydra.main(version_base=None, config_path="configs", config_name="spheres")
+def main(args):
     """Parse command line arguments and execute one of the experiments."""
-    parser = get_default_parser(__doc__)
-    parser.add_argument("--n", type=int, default=512, help="Detected image size")
-    parser.add_argument("--verbose", action="store_true")
-    subparsers = parser.add_subparsers(help="sub-command help", dest="sub-commands", required=True)
-
-    # Creation
-    creation = subparsers.add_parser("create", help="Create spheres")
-    creation.add_argument("output_directory", type=str, help="Output directory")
-    creation.add_argument("--n-spheres", type=int, default=1000, help="Number of spheres")
-    creation.add_argument(
-        "--inner-cylinder-radius",
-        type=int,
-        default=None,
-        help="Maximum lateral distance from the center"
-        "(any part of any sphere will not reach beyond this) [px]"
-    )
-    creation.add_argument("--r-min", type=int, default=2, help="Minimum sphere radius [px]")
-    creation.add_argument("--r-max", type=int, default=12, help="Maximum sphere radius [px]")
-    creation.add_argument(
-        "--rho-min",
-        type=float,
-        default=0.5,
-        help="Minimum relative density (0, 1]"
-    )
-    creation.add_argument(
-        "--rho-max",
-        type=float,
-        default=1.0,
-        help="Maximum relative density (0, 1]"
-    )
-    creation.set_defaults(_func=make_spheres)
-
-    # Projection
-    projection = subparsers.add_parser("project", help="Project spheres")
-    projection.add_argument(
-        "spheres_filename",
-        type=str,
-        help="File with spheres from `create' sub-command"
-    )
-    projection.add_argument("--supersampling", type=int, default=8, help="Supersampling amount")
-    projection.add_argument(
-        "--num-projections",
-        type=int,
-        default=None,
-        help="Number of projections"
-    )
-    projection.set_defaults(_func=project_spheres)
-
-    # X-ray
-    xray = subparsers.add_parser("xray", help="Project spheres")
-    xray.add_argument(
-        "projections_fmt",
-        type=str,
-        help="Projections file name format from `project' sub-command compatible with glob.glob"
-    )
-    xray.add_argument(
-        "--propagation-distance",
-        type=float,
-        default=0,
-        help="Propagation distance [cm]"
-    )
-    xray.add_argument(
-        "--inner-cylinder-radius",
-        type=int,
-        default=None,
-        help="Inner cylinder radius [px]"
-    )
-    xray.add_argument(
-        "--outer-cylinder-radius",
-        type=int,
-        default=None,
-        help="Outer cylinder radius [px]"
-    )
-    xray.add_argument(
-        "--spots-filename",
-        type=str,
-        help="File name with spots for corruption"
-    )
-    xray.add_argument(
-        "--max-absorbed-photons",
-        type=int,
-        default=10000,
-        help="Maximum number of flat-field photons absorbed in the scintillator"
-    )
-    xray.add_argument(
-        "--with-noise",
-        action="store_true",
-        help="Simulate noise or not"
-    )
-    xray.add_argument(
-        "--output-suffix",
-        type=str,
-        help="Output directory will be projections-[output-suffix]"
-    )
-    xray.set_defaults(_func=create_xray_projections)
-
-    args = vars(parser.parse_args())
-
     # Create necessary defaults
-    if "inner_cylinder_radius" in args and not args["inner_cylinder_radius"]:
-        args["inner_cylinder_radius"] = (args["n"] - 100) // 2
+    if not args.inner_cylinder_radius:
+        args.inner_cylinder_radius = (args.n - 100) // 2
 
-    # Clean up parser attributes and verbose output
-    func = args["_func"]
-    del args["_func"]
-    del args["sub-commands"]
+    if args.verbose:
+        print(OmegaConf.to_yaml(args))
 
-    if args["verbose"]:
-        for key, value in args.items():
-            print(f"{key:>20}: {value}")
-
-    del args["verbose"]
-
-    func(**args)
+    if args.run == "create":
+        make_spheres(args)
+    elif args.run == "project":
+        project_spheres(args)
+    elif args.run == "xray":
+        create_xray_projections(args)
 
 
 if __name__ == "__main__":
